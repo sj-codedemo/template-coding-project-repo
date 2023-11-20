ca2da67ab78830f9f0fd5998cad110a7126184d7
sj-codedemo <141835690+sj-codedemo@users.noreply.github.com>
2023-11-20 22:27:34 +0000

    fix: resolve persistent dynamic year generation bug
    - fix(licence): replace incorrect reference '{{cookiecutter.full_name}}' with '{{cookiecutter.author_name}}'
    - refactor(pre_gen_project): remove obsolete dynamic year generation method hook
    - feat(cookiecutter): add 'current_year' entry, using jinja2-time for correct dynamic generation of the current year
    
    This commit successfully resolves the issues with dynamically generating the current year for cookiecutter.json, which previous commits failed to fix.

--- {{cookiecutter.project_slug}}/hooks/pre_gen_project.py
+++ {{cookiecutter.project_slug}}/hooks/pre_gen_project.py
@@ -1,18 +1,2 @@
 #!/usr/bin/env python
 
-import datetime
-import json
-
-# Calculate the current year
-current_year = datetime.datetime.now().year
-
-# Read the existing cookiecutter.json
-with open('cookiecutter.json', 'r') as file:
-    data = json.load(file)
-
-# Add or update the year in the data
-data['current_year'] = current_year
-
-# Write the updated data back to cookiecutter.json
-with open('cookiecutter.json', 'w') as file:
-    json.dump(data, file, indent=4)
